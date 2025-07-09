c58cb7ec1c175492a9a635b211e76f0bc9efff79
ilyes-chabab <ilyes.chabab@laplateforme.io>
2025-07-09 12:00:03 +0200

    ajout du csv

--- kmeans.py
+++ kmeans.py
@@ -1,1 +1,7 @@
 # classe du modèle K-means
+
+import kagglehub
+# Download latest version
+path = kagglehub.dataset_download("imakash3011/customer-personality-analysis")
+
+print("Path to dataset files:", path)
